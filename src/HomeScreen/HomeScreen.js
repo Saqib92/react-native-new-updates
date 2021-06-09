bfc2dd01bb247004d413516e0ab2c1a5168aa50c
saqib92 <najam1992@gmail.com>
2021-06-09 18:24:23 +0500

    camera example

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
- import {
+import {
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -7,19 +7,84 @@ import * as React from 'react';
   Text,
   useColorScheme,
   View,
-  Button
+  Button,
+  TouchableOpacity
 } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
-const HomeScreen = ({navigation}) =>{
-   return (
-     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-       <Text>Home Screen</Text>
-     </View>
-   )
- }
+const HomeScreen = ({ navigation }) => {
 
- function wow(s){
-   alert(s)
- }
+  const takePicture = async function (camera) {
+    const options = { quality: 0.5, base64: true };
+    const data = await camera.takePictureAsync(options);
+    //  eslint-disable-next-line
+    alert(data.uri);
+  };
 
- export default HomeScreen;
+  const PendingView = () => (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'lightgreen',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Waiting</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+
+      >
+        {({ camera, status }) => {
+          if (status !== 'READY') return <PendingView />;
+          return (
+            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+              <TouchableOpacity onPress={() => takePicture(camera)} style={styles.capture}>
+                <Text style={{ fontSize: 14 }}> SNAP </Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      </RNCamera>
+
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+export default HomeScreen;
